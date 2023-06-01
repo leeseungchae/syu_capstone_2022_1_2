aac4f709b84747372508a174d3a71ab266985fa0
chaelee <ztmdco@gmail.com>
2023-06-01 23:28:54 +0900

    [add] auto downlaod, docs

---
+++ docs/source/conf.py
@@ -0,0 +1,55 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../BloomingMind'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'BloomingMind/settings/base.py'
+
+
+project = 'BloomingMind'
+copyright = '2023, chae_lee'
+author = 'chae_lee'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+autodoc_mock_imports = [
+    'django'
+    ]
+
+
+extensions = [
+    'sphinx.ext.autodoc',   # 자동 생성
+    'sphinx.ext.napoleon',  # google docstring
+    'myst_parser'# md 및 확장자 파싱
+    # 'sphinx.ext.viewcode'
+
+]
+napoleon_google_docstring = True
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ko'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
